2c70cb44ef7ba5bc2e5898c97741b90adbdbd5e7
Md. Ariful Islam <arifnislam@gmail.com>
2025-05-03 01:02:29 +0600

    Add common module

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.bitnetika.comndroid"
+    namespace = "com.bitnetika.commons"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.bitnetika.comndroid"
         minSdk = 21
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -37,10 +34,6 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(project(":commons"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
