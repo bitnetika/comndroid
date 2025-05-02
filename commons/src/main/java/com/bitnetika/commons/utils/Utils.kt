505cf5c0c36b57fb1997729dce5ab018d0ad418c
Ariful Islam <mdariful.islam@brainstation-23.com>
2025-05-03 01:02:29 +0600

    Add common module

---
+++ commons/src/main/java/com/bitnetika/commons/utils/Utils.kt
@@ -0,0 +1,5 @@
+package com.bitnetika.commons.utils
+
+fun String?.isEquals(other: String?): Boolean {
+    return equals(other, true)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "comndroid"
 include(":app")
+include(":commons")
