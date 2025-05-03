a6e05447ecd7c601740b8d37bf2e7df3826b095f
Ariful Islam <arifnislam@gmail.com>
2025-05-03 09:18:02 +0600

    Add isContains method which check ignore-case

--- commons/src/main/java/com/bitnetika/commons/utils/Utils.kt
+++ commons/src/main/java/com/bitnetika/commons/utils/Utils.kt
@@ -3,3 +3,11 @@ package com.bitnetika.commons.utils
 fun String?.isEquals(other: String?): Boolean {
     return equals(other, true)
 }
+
+fun CharSequence?.isContains(other: String?): Boolean {
+    if (this == null || other == null) {
+        return false
+    }
+
+    return contains(other, true)
+}
